7ea714909f080902f2ea13a44cc636a95dba2184
Matt Gara <gara.matt@gmail.com>
2020-02-06 22:53:03 -0800

    Much more files to copy.

--- setup_qtcreator/setup_qtcreator.py
+++ setup_qtcreator/setup_qtcreator.py
@@ -2,19 +2,16 @@
 
 import os
 from os.path import expanduser
-import sys
-from shutil import copyfile
+from distutils.dir_util import copy_tree
 
 def mkdir(d):
     if not os.path.exists(d):
         os.mkdir(d)
 
-# copy QtCreator.ini -> ~/.config/QtProject/QtCreator.ini
+# copy QtProject -> ~/.config/QtProject recursively
 home = expanduser("~")
 confd = os.path.join(home,".config") 
 mkdir(confd)
 qtpd = os.path.join(confd,"QtProject") 
-mkdir(qtpd)
-qtinif = os.path.join(qtpd,"QtCreator.ini") 
-copyfile('QtCreator.ini',qtinif)
+copy_tree('QtProject/',qtpd)
 
